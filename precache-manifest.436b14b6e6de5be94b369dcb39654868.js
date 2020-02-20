b26397e36a6c1af22cb8fe9daaed31c4c537abc3
leandronetlink <leandronetlink@gmail.com>
2020-02-20 02:22:46 -0300

    Updates

---
+++ precache-manifest.436b14b6e6de5be94b369dcb39654868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5142299cfbbdbe82f6e71720774525ee",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "4c8b8c02212c2df3cb46",
+    "url": "/my-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed0d64bef62f773be655",
+    "url": "/my-page/static/js/2.0f2f887f.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/my-page/static/js/2.0f2f887f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c8b8c02212c2df3cb46",
+    "url": "/my-page/static/js/main.63bf27b8.chunk.js"
+  },
+  {
+    "revision": "c6f1718e93c25bbf3241",
+    "url": "/my-page/static/js/runtime-main.5b5bb97b.js"
+  }
+]);
